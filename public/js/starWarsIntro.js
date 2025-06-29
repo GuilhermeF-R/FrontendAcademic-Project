9e0edc57cfbebe865dadc2613eec30930b0109ab
GuilhermeF-R <Frodriguesguilherme18@gmail.com>
2025-06-28 23:49:25 -0300

    refatorando mais

--- public/js/starWarsIntro.js
+++ public/js/starWarsIntro.js
@@ -101,3 +101,27 @@ window.addEventListener('load', () => {
     encerrarIntro(); // chama com proteção
   });
 });
+
+// Função para animar a página normalmente
+function animatePageElements() {
+  gsap.utils.toArray('body *').forEach((el) => {
+    if (
+      el.offsetWidth > 0 &&
+      el.offsetHeight > 0 &&
+      !el.closest('#starwars-intro')
+    ) {
+      gsap.from(el, {
+        autoAlpha: 0,
+        y: 20,
+        duration: 0.9,
+        ease: 'power2.out',
+        scrollTrigger: {
+          trigger: el,
+          start: 'top 110%',
+          toggleActions: 'play none none none',
+          once: true
+        }
+      });
+    }
+  });
+}
